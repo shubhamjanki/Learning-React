18e7f611e764aa3ac45e765e1820019b9716682d
Shubham Pandey <shubhamjanki000@gmail.com>
2026-01-17 22:31:11 +0530

    day  react

--- src/App.jsx
+++ src/App.jsx
@@ -5,44 +5,18 @@ import Wrapper from "./Wrapper";
 import Card from "./component/Card";
 import NameCard from "./component/NameCard";
 function App() {
-  const User= [{
-    name: "Shubham",
-    age: "21",
-    email: "shubh@123",
-    id:1
-  },
- {
-    name: "karan",
-    age: "21",
-    email: "shubh@123",
-    id:2
-  },
-  {
-    name: "Sainik",
-    age: "21",
-    email: "shubh@123",
-    id:3
-  },
-   {
-    name: "ayush",
-    age: "21",
-    email: "shubh@123",
-    id:4
-  }];
-  function array(newname) {
-    setname([...name, newname]);
-    console.log(name);
-  }
+  const [counter,setcounter]=useState(0);
+  
+  function callonce(){
+    setcounter(counter+1);
+  }  
+  callonce();
   return (
     <>
       <div>
         <h1>User Table</h1>
-        <Card userData={User}></Card>
-        {
-          User.map(user => (
-            <NameCard color="red"key={user.id} userData={user}></NameCard>
-          ))
-        }
+        <button onClick={callonce}>Increment {counter}</button>
+
       </div>
   
     </>
